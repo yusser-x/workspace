4795fe22bf769bdcde45dbe9d33ba51dc1234cc8
yusser-x <192161@alu.murciaeduca.es>
2025-02-15 17:44:21 +0100

    Subir el archivo prueba para saber si el repositorio funciona correctamente

---
+++ Prueba/src/Prueba/Instruccion.java
@@ -0,0 +1,11 @@
+package Prueba;
+
+public class Instruccion {
+
+	public static void main(String[] args) {
+
+		System.out.println("Aqui comprobamos si el repositorio funciona como queria");
+
+	}
+
+}
---
+++ Prueba/src/Prueba/Resultado.java
@@ -0,0 +1,15 @@
+package Prueba;
+
+public class Resultado {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Aqui vamos a hacer una pequeña suma a la vieja usanza");
+		int num = 4;
+		int num1 = 5;
+		int resultado = num + num1;
+		System.out.println(resultado);
+
+	}
+
+}
